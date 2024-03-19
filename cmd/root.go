8436a28e53dcd6156a40f3a1a3e7637cc3627544
Evhen <malysh.evgeniy@gmail.com>
2024-03-19 21:58:01 +0200

    fix root description

--- cmd/root.go
+++ cmd/root.go
@@ -10,13 +10,8 @@ var objectHash string
 
 var rootCmd = &cobra.Command{
 	Use:   "mygit",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A CLI tool to interact with a git repository",
+	Long:  `A CLI tool to interact with a git repository`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
